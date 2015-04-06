0972ff029ce202929fb4c7db8ac65119e18c03e4
zass30 <jbeasley@gmail.com>
2015-04-06 13:44:06 -0700

    added skeleton armies structure

---
+++ SharpChess.Model/Armies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpChess.Model
+{
+    class Armies
+    {
+
+        #region Enums
+        public enum Armies
+        {
+            Classic,
+            Chancellor
+        }
+        #endregion
+    }
+}
